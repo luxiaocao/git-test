33417da855c2c96d0001adb74d85718b98fc4164
luxiaocao <48720574+luxiaocao@users.noreply.github.com>
2022-06-19 22:29:52 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("dev -hello git4");
         System.out.println("dev -hello git5");
+        System.out.println("pull test");
     }
 }
